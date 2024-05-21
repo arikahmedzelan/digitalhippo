9927cfad651be48121aa857a091ca138868a4a06
Arik Ahmed Zelan <83626894+arikahmedzelan@users.noreply.github.com>
2024-05-21 18:45:33 +0600

    Home page work started
    Home page work started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
-  return (
-    <p>hello wrold!</p>
-    
-  )
+  return <MaxWidthWrapper>
+    <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+      <h1 className="text-4xl font-bold tracking-tighter text-gray-900 sm:text-6xl">
+        Your marketplace for high-quality{' '}
+      <span className="text-blue-600">
+        Digital assets
+        </span>
+        .
+      </h1>
+
+      <p className="mt-6 text-lg max-w-prose text-muted-foreground">Welcome to Imaginio. Every asset on our
+            platform is verified by our team to ensure our
+            highest quality standards.</p>
+
+      <div></div>
+    </div>
+  </MaxWidthWrapper>
 }
+ 
